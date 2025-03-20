9e96e44508c3bd6b14a7f41f088ad5a7e45139b6
Marcos Aurelio Wozhiak Junior de Oliveira <aureliowozhiak@gmail.com>
2025-03-20 09:09:46 -0300

    update

--- realtime_events/db/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE event_metrics (
-    event_type VARCHAR(50),
-    event_count INT,
-    unique_users INT
-);
--- realtime_events/kafka/create_topic.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker exec kafka kafka-topics.sh --create --topic user_events --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
